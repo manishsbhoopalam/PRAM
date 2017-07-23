3c7bcb4cadfdb50e194e4c3b3bc9be66ebbf44e2
manishsbhoopalam <manishsbhoopalam@gmail.com>
2017-07-23 17:28:33 +0530

    Implemented IntegerAggregator.java, Aggregate.java,Predicate.java,JoinPredicate.java and Filter.java
    These files were implemented by Ram Adithya. I'm uploading these as he was busy with in the Hostel Office with registration.

--- src/java/simpledb/Aggregate.java
+++ src/java/simpledb/Aggregate.java
@@ -11,6 +11,13 @@ public class Aggregate extends Operator {
 
     private static final long serialVersionUID = 1L;
 
+    private DbIterator m_tupleIterator;
+    private int m_aggregateFieldIndex;
+    private int m_groupByFieldIndex;
+    private Aggregator.Op m_op;
+    
+    private Aggregator m_aggregator;
+    private DbIterator m_aggregateIterator;
     /**
      * Constructor.
      * 
@@ -31,6 +38,31 @@ public class Aggregate extends Operator {
      */
     public Aggregate(DbIterator child, int afield, int gfield, Aggregator.Op aop) {
 	// some code goes here
+	m_tupleIterator = child;
+    	m_aggregateFieldIndex = afield;
+    	m_groupByFieldIndex = gfield;
+    	m_op = aop;
+    	m_aggregateIterator = null;
+    	
+    	Type groupByType;
+    	
+    	if (m_groupByFieldIndex == Aggregator.NO_GROUPING)
+    		groupByType = null;
+    	else 
+    		groupByType = m_tupleIterator.getTupleDesc().getFieldType(m_groupByFieldIndex);
+    	
+    	Type aggregateType = m_tupleIterator.getTupleDesc().getFieldType(m_aggregateFieldIndex);
+    	switch(aggregateType)
+    	{
+    		case INT_TYPE: 
+    			m_aggregator = new IntegerAggregator(m_groupByFieldIndex, groupByType, m_aggregateFieldIndex, m_op);
+    			break;
+    		case STRING_TYPE:
+    			m_aggregator = new StringAggregator(m_groupByFieldIndex, groupByType, m_aggregateFieldIndex, m_op);
+    			break;
+    		default:
+    			assert(false);
+	}
     }
 
     /**
@@ -40,7 +72,7 @@ public class Aggregate extends Operator {
      * */
     public int groupField() {
 	// some code goes here
-	return -1;
+	return m_groupByFieldIndex;
     }
 
     /**
@@ -50,7 +82,9 @@ public class Aggregate extends Operator {
      * */
     public String groupFieldName() {
 	// some code goes here
-	return null;
+	if (this.groupField() == Aggregator.NO_GROUPING)
+	    return null;
+	return m_tupleIterator.getTupleDesc().getFieldName(this.groupField());
     }
 
     /**
@@ -58,7 +92,7 @@ public class Aggregate extends Operator {
      * */
     public int aggregateField() {
 	// some code goes here
-	return -1;
+	return m_aggregateFieldIndex;
     }
 
     /**
@@ -67,7 +101,7 @@ public class Aggregate extends Operator {
      * */
     public String aggregateFieldName() {
 	// some code goes here
-	return null;
+	return m_tupleIterator.getTupleDesc().getFieldName(this.aggregateField());
     }
 
     /**
@@ -75,7 +109,7 @@ public class Aggregate extends Operator {
      * */
     public Aggregator.Op aggregateOp() {
 	// some code goes here
-	return null;
+	return m_op;
     }
 
     public static String nameOfAggregatorOp(Aggregator.Op aop) {
@@ -85,6 +119,14 @@ public class Aggregate extends Operator {
     public void open() throws NoSuchElementException, DbException,
 	    TransactionAbortedException {
 	// some code goes here
+	super.open();
+    	m_tupleIterator.open();
+    	while (m_tupleIterator.hasNext())
+    	{
+    		m_aggregator.mergeTupleIntoGroup(m_tupleIterator.next());
+    	}
+    	m_aggregateIterator = m_aggregator.iterator();
+	m_aggregateIterator.open();
     }
 
     /**
@@ -96,11 +138,12 @@ public class Aggregate extends Operator {
      */
     protected Tuple fetchNext() throws TransactionAbortedException, DbException {
 	// some code goes here
-	return null;
+	return m_aggregateIterator.hasNext() ? m_aggregateIterator.next() : null;
     }
 
     public void rewind() throws DbException, TransactionAbortedException {
 	// some code goes here
+	m_aggregateIterator.rewind();
     }
 
     /**
@@ -116,22 +159,26 @@ public class Aggregate extends Operator {
      */
     public TupleDesc getTupleDesc() {
 	// some code goes here
-	return null;
+	return m_tupleIterator.getTupleDesc();
     }
 
     public void close() {
 	// some code goes here
+	super.close();
+    	m_tupleIterator.close();
+	m_aggregateIterator.close();
     }
 
     @Override
     public DbIterator[] getChildren() {
 	// some code goes here
-	return null;
+	return new DbIterator[] {m_aggregateIterator};
     }
 
     @Override
     public void setChildren(DbIterator[] children) {
 	// some code goes here
+	m_aggregateIterator = children[0];
     }
     
 }
